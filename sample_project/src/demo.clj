2c686465afcbc4acf467a4d05d0ebcc4d5079fd5
Andrea Crotti <andrea.crotti.0@gmail.com>
2019-12-17 15:50:59 +0000

    add explicit dep and demo commands outputs

--- sample_project/src/demo.clj
+++ sample_project/src/demo.clj
@@ -1,5 +1,6 @@
 (ns demo
   (:require [topology-grapher.describe :as td]
+            [topology-grapher.topics :as t]
             [describe :as d]))
 
 (def topologies
@@ -10,11 +11,15 @@
 ;; query information about the topologies
 (comment
   (t/topology->loom sample-topology))
+;; => #loom.graph.BasicEditableDigraph{:nodeset #{"output" "input" "my-application-id"}, :adj {"input" #{"my-application-id"}, "my-application-id" #{"output"}}, :in {"my-application-id" #{"input"}, "output" #{"my-application-id"}}}
+;; => nil
 
 ;; now extract the topics
 (comment
   (t/extract-topics sample-topology))
+;; => {:inputs #{"input"}, :outputs #{"output"}}
 
 ;; render all the topologies available
 (comment
   (d/render-all!))
+;; => "/tmp/graphs/detail_26671937151c7d7fec4c545264f060bc.png"
